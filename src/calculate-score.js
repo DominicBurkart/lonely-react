bc18ac9d9e12fc9cf523428902a45dcd27123dab
Dominic Burkart <1dominicb@gmail.com>
2018-04-28 19:01:48 -0400

    Revert "added autism quotient"
    This reverts commit b6d13ac247bf9d6f9ed156e518497619522fb98b.

--- src/calculate-score.js
+++ src/calculate-score.js
@@ -11,34 +11,21 @@ export default function calculateScore(taskName, data) {
       recodedData[item.replace(rawItemRegEx, "")] = data[item].value
     }
   })
+  // recode the items that need to be reverse coded
+  const recodeItems = [1, 5, 6, 9, 10, 15, 16, 19, 20]
+  recodeItems.map((item) => {
+      const rawValue = data[taskName + '_' + item].value
+      const recodedValue = 5 - rawValue
 
-  //rescore agree = 1 values
-  const agr = [1, 3, 6, 7, 8, 11, 12, 15, 17] //indices (base 0 question #s)
-  const agr_d = {4:1, 3:1, 2:0, 1:0} //recode keys
-
-  //rescore disagree = 1 values
-  const disagr = [0, 2, 4, 5, 9, 10, 13, 14, 16, 18, 19] //indices
-  const disagr_d = {4:0, 3:0, 2:1, 1:1} // recode keys
-
-  for (var i = 0; i < data.length; i += 1){
-    const val = data[taskName+"_"+i]
-    if (i in agr){
-      recodedData[i] = agr_d[val].value
+      recodedData[item] = recodedValue
     }
-    else if (i in disagr) {
-      recodedData[i] = disagr_d[val].value
-    }
-    else{
-      console.log("Indexing error.")
-    }
-  }
+  )
 
   // create new object for summary values
-  let scaleSummary = {plotdata: {low: 28.5, high: 46.58, average: 40.08}}
+  let lonelySummary = {plotdata: {low: 28.5, high: 46.58, average: 40.08}}
   const totalScore = Object.values(recodedData).reduce((a,b) => { return parseInt(a, 10) + parseInt(b, 10) })
-  scaleSummary.plotdata.score = totalScore
+  lonelySummary.plotdata.score = totalScore
 
-  const fullResult = Object.assign({}, data, scaleSummary)
-  console.log(fullResult)
+  const fullResult = Object.assign({}, data, lonelySummary)
   return {[taskName]: fullResult}
 }
