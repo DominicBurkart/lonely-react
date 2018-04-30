ac89bf021abe4cccad12cfabb893e534ca8d9e0f
Dominic Burkart <1dominicb@gmail.com>
2018-04-30 09:02:18 -0400

    autism spectrum questionnaire final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Scale from "./autism"
 import calculateScore from "./calculate-score"
 import find from 'lodash/find'
@@ -9,64 +9,46 @@ const userID = document.getElementById('user_id').innerHTML
 const studyID = document.getElementById('study_id').innerHTML
 
 class App extends Component {
-  constructor() {
-    super()
-    this.endTask = this.endTask.bind(this)
-    this.state = {isFinished: false}
-  }
-  endTask(name, data) {
-    this.finalResult = calculateScore(name, data)
-    const postData = Object.assign({}, this.finalResult['generic_scale'].plotdata, {user_id: userID,
-      study_id: studyID})
-    $.post({
-      url: "/studies.json",
-      headers: {
-        'X-CSRF-Token': this.getCSRFToken(),
-      },
-      dataType: 'json',
-      data: {
-        study: postData
-      }
-    })
-      .then(returnValue => window.location = returnValue.redirect_url)
-    /*const postData = {
-      method: 'POST',
-      headers: {
-        'X-CSRF-Token': this.getCSRFToken()
-      },
-      dataType: 'json',
-      data: {
-        study: {
-          user_id: userID,
-          study_id: studyID,
-          custom_study_results: this.finalResult,
-          score: this.finalResult.lonely.plotdata.score
-        }
-      }
+    constructor() {
+        super()
+        this.endTask = this.endTask.bind(this)
+        this.state = {isFinished: false}
     }
-    fetch('/studies.json', postData)
-      .then(() => console.log(postData, this.getCSRFToken()))*/
 
-    this.setState({isFinished: true})
-  }
-  getCSRFToken() {
-    return find(document.getElementsByTagName("meta"), (meta) => {
-      return meta.name === "csrf-token"
-    }).content
-  }
-  render() {
-    return (
-      this.state.isFinished ? (
-        <div>
-          Questionnaire completed. <br /> JSON data:
-          <br />
-          <pre>{JSON.stringify(this.finalResult, null, 2)}</pre>
-        </div>
-      ) : (
-        <Scale endTask={this.endTask}/>
-      )
-    );
-  }
+    endTask(name, data) {
+        this.finalResult = calculateScore(name, data)
+        const postData = Object.assign({},
+            this.finalResult,
+            {user_id: userID, study_id: studyID})
+        console.log(postData);
+        // $.post({
+        //     url: "/studies.json",
+        //     headers: {
+        //         'X-CSRF-Token': this.getCSRFToken(),
+        //     },
+        //     dataType: 'json',
+        //     data: {
+        //         study: postData
+        //     }
+        // }).then(returnValue => window.location = returnValue.redirect_url)
+        // this.setState({isFinished: true})
+    }
+
+    getCSRFToken() {
+        return find(document.getElementsByTagName("meta"), (meta) => {
+            return meta.name === "csrf-token"
+        }).content
+    }
+
+    render() {
+        return (
+            this.state.isFinished ? (
+                <div></div>
+            ) : (
+                <Scale endTask={this.endTask}/>
+            )
+        );
+    }
 }
 
 export default App;
--- src/calculate-score.js
+++ src/calculate-score.js
@@ -1,44 +1,69 @@
-
 export default function calculateScore(taskName, data) {
 
-  // get regex for item prefix based on task name
-  const rawItemRegEx = RegExp(taskName + "_")
+    // get regex for item prefix based on task name
+    const rawItemRegEx = RegExp(taskName + "_")
 
-  // add each item's value to new object
-  let recodedData = {}
-  Object.keys(data).map((item) => {
-    if (rawItemRegEx.test(item)){
-      recodedData[item.replace(rawItemRegEx, "")] = data[item].value
+    // add each item's value to new object
+    let recodedData = {}
+    Object.keys(data).map((item) => {
+        if (rawItemRegEx.test(item)) {
+            recodedData[item.replace(rawItemRegEx, "")] = data[item].value
+        }
+    })
+
+    //rescore agree = 1 values
+    const agr = [1, 3, 6, 7, 8, 11, 12, 15, 17] //indices (base 0 question #s)
+    const agr_d = {"4": 1, "3": 1, "2": 0, "1": 0} //recode keys
+
+    //rescore disagree = 1 values
+    const disagr = [0, 2, 4, 5, 9, 10, 13, 14, 16, 18, 19] //indices
+    const disagr_d = {"4": 0, "3": 0, "2": 1, "1": 1} // recode keys
+
+    console.log("data");
+    console.log(data);
+    const l = Object.keys(data).length;
+    for (var i = 0; i < l; i++) {
+        let d = "generic_scale_"+(i+1);
+        console.log(d)
+        console.log(data[d])
+        const val = data[d].value
+        if (agr.includes(i)) {
+            console.log(i + " in agr")
+            recodedData[i.toString()] = agr_d[val.toString()]
+        }
+        else if (disagr.includes(i)) {
+            console.log(i + " in disagr")
+            recodedData[i.toString()] = disagr_d[val.toString()]
+        }
+        else {
+            console.log("Indexing error.");
+            console.log(i);
+            console.log(val)
+        }
+        console.log(recodedData)
     }
-  })
 
-  //rescore agree = 1 values
-  const agr = [1, 3, 6, 7, 8, 11, 12, 15, 17] //indices (base 0 question #s)
-  const agr_d = {4:1, 3:1, 2:0, 1:0} //recode keys
+    delete recodedData["20"];
+    console.log("reformatted data");
+    console.log(recodedData);
 
-  //rescore disagree = 1 values
-  const disagr = [0, 2, 4, 5, 9, 10, 13, 14, 16, 18, 19] //indices
-  const disagr_d = {4:0, 3:0, 2:1, 1:1} // recode keys
-
-  for (var i = 0; i < data.length; i += 1){
-    const val = data[taskName+"_"+i]
-    if (i in agr){
-      recodedData[i] = agr_d[val].value
+    function pull_out(x) {
+        var keys = Object.keys(x);
+        var vals = keys.map(k => x[k].value);
+        var out = [];
+        for (var i = 0; i < keys.length; i++) {
+            out.push({[keys[i]]: vals[i]});
+        }
+        return out;
     }
-    else if (i in disagr) {
-      recodedData[i] = disagr_d[val].value
-    }
-    else{
-      console.log("Indexing error.")
-    }
-  }
 
-  // create new object for summary values
-  let scaleSummary = {plotdata: {low: 28.5, high: 46.58, average: 40.08}}
-  const totalScore = Object.values(recodedData).reduce((a,b) => { return parseInt(a, 10) + parseInt(b, 10) })
-  scaleSummary.plotdata.score = totalScore
-
-  const fullResult = Object.assign({}, data, scaleSummary)
-  console.log(fullResult)
-  return {[taskName]: fullResult}
+    // create new object for summary values
+    console.log(data);
+    const formatted_data = pull_out(data);
+    console.log(formatted_data);
+    const s = Object.values(recodedData).reduce((a, b) => {
+        return parseInt(a, 10) + parseInt(b, 10)
+    });
+    const fullResult = {score: s, custom_study_results: JSON.stringify(formatted_data)};
+    return fullResult
 }
